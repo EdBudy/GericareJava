31858150ecb01db1e97ffc094740f0335955b6de
Santiago <santiagosossa0629@gmail.com>
2025-09-27 10:24:01 -0500

    Controller Excel

--- src/main/java/com/example/Gericare/Controller/UsuarioController.java
+++ src/main/java/com/example/Gericare/Controller/UsuarioController.java
@@ -5,9 +5,16 @@ import com.example.Gericare.Service.UsuarioService;
 import com.example.Gericare.entity.Cuidador;
 import com.example.Gericare.entity.Familiar;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity; // Para respuestas HTTP
 import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
