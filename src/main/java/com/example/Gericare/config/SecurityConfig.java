888db6aae2d6e6d9720045db867233381ba287e8
Andres Castro <castrosanabriaandresfelipe@gmail.com>
2025-10-04 01:02:44 -0500

    cmnt

--- src/main/java/com/example/Gericare/config/SecurityConfig.java
+++ src/main/java/com/example/Gericare/config/SecurityConfig.java
@@ -19,38 +19,44 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder(); // BCryptPasswordEncoder para que las contraseñas en la bd se guarden hasheadas
     }
 
-    @Bean
+    @Bean // Spring detecta este objeto y lo usa automáticamente como la configuración de seguridad
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                // Configuración de autorización de peticiones HTTP
                 .authorizeHttpRequests(authorize -> authorize
-                        // Permisos públicos
+                        // Permisos públicos (no requieren login)
                         .requestMatchers("/", "/login", "/registro", "/reset-password", "/css/**", "/js/**", "/images/**")
                         .permitAll()
 
                         // Reglas cuidador
                         .requestMatchers(HttpMethod.POST, "/actividades/completar/**").hasRole("Cuidador")
 
-                        // Ver lista actividad
+                        // Ver lista actividad (cuidador)
                         .requestMatchers("/actividades/actividades-pacientes").hasRole("Cuidador")
 
                         // Reglas admin
                         .requestMatchers("/usuarios/**", "/pacientes/**", "/actividades/**")
                         .hasRole("Administrador")
 
-                        // Permisos para las otras vistas de cuidador y familiar
+                        // Vistas cuidador
                         .requestMatchers("/cuidador/**").hasRole("Cuidador")
+                        // Vistas familiar
                         .requestMatchers("/familiar/**").hasRole("Familiar")
 
+                        // Las demás rutas requieren estar autenticado (logueado)
                         .anyRequest().authenticated())
+                // Config del login
                 .formLogin(form -> form
                         .loginPage("/login") // Se le dice a Spring cuál es la pag de login
                         .defaultSuccessUrl("/dashboard", true) // Redirigir si el login es exitoso
                         .permitAll())
+                // Config del logout
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout")
+                        .logoutSuccessUrl("/login?logout") // Redirige a login después de cerrar sesión
                         .permitAll());
 
+        // Devolver el filtro de seguridad construido
         return http.build();
     }
 }
