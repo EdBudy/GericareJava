1ff856fc537639c43ea4b96adb028117ff2fa8c4
Andres Castro <castrosanabriaandresfelipe@gmail.com>
2025-11-13 16:02:08 -0500

    tabla y filtros

--- src/main/java/com/example/Gericare/specification/UsuarioSpecification.java
+++ src/main/java/com/example/Gericare/specification/UsuarioSpecification.java
@@ -16,9 +16,26 @@ public class UsuarioSpecification {
             List<Predicate> predicates = new ArrayList<>();
             predicates.add(criteriaBuilder.equal(root.get("estado"), EstadoUsuario.Activo));
 
+            // Búsqueda por nombre o apellido
             if (nombre != null && !nombre.isEmpty()) {
-                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("nombre")), "%" + nombre.toLowerCase() + "%"));
+                String likePattern = "%" + nombre.toLowerCase() + "%";
+
+                // Crea predicado para buscar en 'nombre'
+                Predicate nombreLike = criteriaBuilder.like(
+                        criteriaBuilder.lower(root.get("nombre")),
+                        likePattern
+                );
+
+                // Crea predicado para buscar en 'apellido'
+                Predicate apellidoLike = criteriaBuilder.like(
+                        criteriaBuilder.lower(root.get("apellido")),
+                        likePattern
+                );
+
+                // Combina ambos con un OR
+                predicates.add(criteriaBuilder.or(nombreLike, apellidoLike));
             }
+
             if (documento != null && !documento.isEmpty()) {
                 predicates.add(criteriaBuilder.like(root.get("documentoIdentificacion"), "%" + documento + "%"));
             }
@@ -26,7 +43,7 @@ public class UsuarioSpecification {
                 predicates.add(criteriaBuilder.equal(root.get("rol").get("rolNombre"), rol));
             }
 
-            // Excluir siempre a los administradores de la lista
+            // Excluir administradores de la lista
             predicates.add(criteriaBuilder.notEqual(root.get("rol").get("rolNombre"), RolNombre.Administrador));
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
